11510961d0fcaa856b9dd0f4616bc8a4f3df05e3
Nov11 <529079634@qq.com>
2018-02-12 23:58:58 +0800

    2.2 IntegerAggregator done. tests passed.

--- src/java/simpledb/Aggregate.java
+++ src/java/simpledb/Aggregate.java
@@ -13,78 +13,73 @@ public class Aggregate extends Operator {
 
     /**
      * Constructor.
-     * 
+     * <p>
      * Implementation hint: depending on the type of afield, you will want to
      * construct an {@link IntAggregator} or {@link StringAggregator} to help
      * you with your implementation of readNext().
-     * 
-     * 
-     * @param child
-     *            The DbIterator that is feeding us tuples.
-     * @param afield
-     *            The column over which we are computing an aggregate.
-     * @param gfield
-     *            The column over which we are grouping the result, or -1 if
-     *            there is no grouping
-     * @param aop
-     *            The aggregation operator to use
+     *
+     * @param child  The DbIterator that is feeding us tuples.
+     * @param afield The column over which we are computing an aggregate.
+     * @param gfield The column over which we are grouping the result, or -1 if
+     *               there is no grouping
+     * @param aop    The aggregation operator to use
      */
     public Aggregate(DbIterator child, int afield, int gfield, Aggregator.Op aop) {
-	// some code goes here
+        // some code goes here
     }
 
     /**
      * @return If this aggregate is accompanied by a groupby, return the groupby
-     *         field index in the <b>INPUT</b> tuples. If not, return
-     *         {@link simpledb.Aggregator#NO_GROUPING}
-     * */
+     * field index in the <b>INPUT</b> tuples. If not, return
+     * {@link simpledb.Aggregator#NO_GROUPING}
+     */
     public int groupField() {
-	// some code goes here
-	return -1;
+        // some code goes here
+        return -1;
     }
 
     /**
      * @return If this aggregate is accompanied by a group by, return the name
-     *         of the groupby field in the <b>OUTPUT</b> tuples If not, return
-     *         null;
-     * */
+     * of the groupby field in the <b>OUTPUT</b> tuples If not, return
+     * null;
+     */
     public String groupFieldName() {
-	// some code goes here
-	return null;
+        // some code goes here
+        return null;
     }
 
     /**
      * @return the aggregate field
-     * */
+     */
     public int aggregateField() {
-	// some code goes here
-	return -1;
+        // some code goes here
+        return -1;
     }
 
     /**
      * @return return the name of the aggregate field in the <b>OUTPUT</b>
-     *         tuples
-     * */
+     * tuples
+     */
     public String aggregateFieldName() {
-	// some code goes here
-	return null;
+        // some code goes here
+        return null;
     }
 
     /**
      * @return return the aggregate operator
-     * */
+     */
     public Aggregator.Op aggregateOp() {
-	// some code goes here
-	return null;
+        // some code goes here
+        return null;
     }
 
     public static String nameOfAggregatorOp(Aggregator.Op aop) {
-	return aop.toString();
+        return aop.toString();
     }
 
     public void open() throws NoSuchElementException, DbException,
-	    TransactionAbortedException {
-	// some code goes here
+            TransactionAbortedException {
+        // some code goes here
     }
 
     /**
@@ -95,12 +90,12 @@ public class Aggregate extends Operator {
      * aggregate. Should return null if there are no more tuples.
      */
     protected Tuple fetchNext() throws TransactionAbortedException, DbException {
-	// some code goes here
-	return null;
+        // some code goes here
+        return null;
     }
 
     public void rewind() throws DbException, TransactionAbortedException {
-	// some code goes here
+        // some code goes here
     }
 
     /**
@@ -108,30 +103,30 @@ public class Aggregate extends Operator {
      * this will have one field - the aggregate column. If there is a group by
      * field, the first field will be the group by field, and the second will be
      * the aggregate value column.
-     * 
+     * <p>
      * The name of an aggregate column should be informative. For example:
      * "aggName(aop) (child_td.getFieldName(afield))" where aop and afield are
      * given in the constructor, and child_td is the TupleDesc of the child
      * iterator.
      */
     public TupleDesc getTupleDesc() {
-	// some code goes here
-	return null;
+        // some code goes here
+        return null;
     }
 
     public void close() {
-	// some code goes here
+        // some code goes here
     }
 
     @Override
     public DbIterator[] getChildren() {
-	// some code goes here
-	return null;
+        // some code goes here
+        return null;
     }
 
     @Override
     public void setChildren(DbIterator[] children) {
-	// some code goes here
+        // some code goes here
     }
-    
+
 }
--- src/java/simpledb/IntegerAggregator.java
+++ src/java/simpledb/IntegerAggregator.java
@@ -1,54 +1,178 @@
 package simpledb;
 
+import java.util.*;
+
 /**
  * Knows how to compute some aggregate over a set of IntFields.
  */
 public class IntegerAggregator implements Aggregator {
 
     private static final long serialVersionUID = 1L;
+    private int groupingField;
+    private Type groupingFieldType;
+    private int fieldId;
+    private Op operaion;
+    private Map<Field, List<Integer>> hash;
 
     /**
      * Aggregate constructor
-     * 
-     * @param gbfield
-     *            the 0-based index of the group-by field in the tuple, or
-     *            NO_GROUPING if there is no grouping
-     * @param gbfieldtype
-     *            the type of the group by field (e.g., Type.INT_TYPE), or null
-     *            if there is no grouping
-     * @param afield
-     *            the 0-based index of the aggregate field in the tuple
-     * @param what
-     *            the aggregation operator
+     *
+     * @param gbfield     the 0-based index of the group-by field in the tuple, or
+     *                    NO_GROUPING if there is no grouping
+     * @param gbfieldtype the type of the group by field (e.g., Type.INT_TYPE), or null
+     *                    if there is no grouping
+     * @param afield      the 0-based index of the aggregate field in the tuple
+     * @param what        the aggregation operator
      */
 
     public IntegerAggregator(int gbfield, Type gbfieldtype, int afield, Op what) {
         // some code goes here
+        this.groupingField = gbfield;
+        this.groupingFieldType = gbfieldtype;
+        this.fieldId = afield;
+        this.operaion = what;
+        hash = new HashMap<>();
     }
 
     /**
      * Merge a new tuple into the aggregate, grouping as indicated in the
      * constructor
-     * 
-     * @param tup
-     *            the Tuple containing an aggregate field and a group-by field
+     *
+     * @param tup the Tuple containing an aggregate field and a group-by field
      */
     public void mergeTupleIntoGroup(Tuple tup) {
         // some code goes here
+        List<Integer> value = null;
+        if (groupingField == NO_GROUPING) {
+            value = hash.computeIfAbsent(null, k -> new ArrayList<>());
+        } else {
+            Field field = tup.getField(groupingField);
+            value = hash.computeIfAbsent(field, k-> new ArrayList<>());
+        }
+        assert tup.getField(fieldId) instanceof IntField;
+        IntField afield = (IntField) tup.getField(fieldId);
+        value.add(afield.getValue());
     }
 
     /**
      * Create a DbIterator over group aggregate results.
-     * 
+     *
      * @return a DbIterator whose tuples are the pair (groupVal, aggregateVal)
-     *         if using group, or a single (aggregateVal) if no grouping. The
-     *         aggregateVal is determined by the type of aggregate specified in
-     *         the constructor.
+     * if using group, or a single (aggregateVal) if no grouping. The
+     * aggregateVal is determined by the type of aggregate specified in
+     * the constructor.
      */
     public DbIterator iterator() {
         // some code goes here
-        throw new
-        UnsupportedOperationException("please implement me for proj2");
+//        throw new
+//                UnsupportedOperationException("please implement me for proj2");
+        TupleDesc tupleDesc = null;
+        if (groupingField == NO_GROUPING) {
+            tupleDesc = new TupleDesc(new Type[]{Type.INT_TYPE});
+        } else {
+            tupleDesc = new TupleDesc(new Type[]{groupingFieldType, Type.INT_TYPE});
+        }
+        return new IntAggregateIter(this, tupleDesc);
     }
 
+    static class IntAggregateIter implements DbIterator {
+
+        private boolean open = false;
+        private IntegerAggregator integerAggregator;
+        private TupleDesc desc;
+        private Iterator<Map.Entry<Field, List<Integer>>> iterator;
+
+        public IntAggregateIter(IntegerAggregator integerAggregator, TupleDesc desc) {
+            this.integerAggregator = integerAggregator;
+            this.desc = desc;
+        }
+
+        @Override
+        public void open() throws DbException, TransactionAbortedException {
+            open = true;
+            iterator = integerAggregator.hash.entrySet().iterator();
+        }
+
+        @Override
+        public boolean hasNext() throws DbException, TransactionAbortedException {
+            return iterator.hasNext();
+        }
+
+        private Integer compute(Op op, List<Integer> list) {
+            assert list.size() > 0;
+            Integer result = 0;
+            switch (op) {
+                case AVG: {
+                    result = 0;
+                    for (Integer item : list) {
+                        result += item;
+                    }
+                    result /= list.size();
+                }
+                break;
+                case MIN: {
+                    result = list.get(0);
+                    for (Integer item : list) {
+                        result = Math.min(result, item);
+                    }
+
+                }
+                break;
+                case MAX: {
+                    result = list.get(0);
+                    for (Integer item : list) {
+                        result = Math.max(result, item);
+                    }
+
+                }
+                break;
+                case COUNT: {
+                    result = list.size();
+                }
+                break;
+                case SUM: {
+                    result = 0;
+                    for (Integer item : list) {
+                        result += item;
+                    }
+                }
+                break;
+                default:
+                    assert false;
+            }
+            return result;
+        }
+
+        @Override
+        public Tuple next() throws DbException, TransactionAbortedException, NoSuchElementException {
+            if (!hasNext()) {
+                throw new NoSuchElementException();
+            }
+            Map.Entry<Field, List<Integer>> entry = iterator.next();
+            Tuple result = new Tuple(desc);
+            Integer ret = compute(integerAggregator.operaion, entry.getValue());
+            if (integerAggregator.groupingField == NO_GROUPING) {
+                result.setField(0, new IntField(ret));
+            } else {
+                result.setField(0, entry.getKey());
+                result.setField(1, new IntField(ret));
+            }
+            return result;
+        }
+
+        @Override
+        public void rewind() throws DbException, TransactionAbortedException {
+            iterator = integerAggregator.hash.entrySet().iterator();
+        }
+
+        @Override
+        public TupleDesc getTupleDesc() {
+            return desc;
+        }
+
+        @Override
+        public void close() {
+            open = false;
+        }
+    }
 }
